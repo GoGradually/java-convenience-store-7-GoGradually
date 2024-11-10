46a9f2f988e5300fb6733bf5a970c2ce2d67b933
GoGradually <chkun3109@gmail.com>
2024-11-10 21:40:46 +0900

    feat(Promotion.java): 프로모션의 상속관계 구조 변경
    - NullPromotion 객체 생성을 위해, Promotion을 인터페이스로 변경 후, PromotionImpl을 구현체로 사용하도록 변경

--- src/main/java/store/Promotion.java
+++ src/main/java/store/Promotion.java
@@ -2,45 +2,13 @@ package store;
 
 import java.time.LocalDate;
 
-public class Promotion {
-    private String promotionName;
-    private int buy;
-    private int get;
-    private LocalDate startDate;
-    private LocalDate endDate;
+public interface Promotion {
 
-    public Promotion(String promotionName, int buy, int get, LocalDate startDate, LocalDate endDate) {
-        validatePromotion(promotionName, buy, get, startDate, endDate);
-        this.promotionName = promotionName;
-        this.buy = buy;
-        this.get = get;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
+    int getBuy();
 
-    public int getBuy() {
-        return buy;
-    }
+    boolean isPromotable(LocalDate date);
 
-    public boolean isPromotable(LocalDate date) {
-        return !date.isAfter(endDate) && !date.isBefore(startDate);
-    }
+    int getAddableAmount(int offer);
 
-    public int getAddableAmount(int offer) {
-        return Math.max((offer + get) - (offer + get) % (buy + get) - offer, 0);
-    }
-
-    public int getFreeAmount(int offer) {
-        return offer / (buy + get) * get;
-    }
-
-    private void validatePromotion(String promotionName, int buy, int get, LocalDate startDate, LocalDate endDate) {
-        if (promotionName == null || promotionName.isEmpty() || promotionName.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 프로모션을 등록하는 도중 예외가 발생했습니다.");
-        } else if (buy < 1 || buy > 10 || get != 1) {
-            throw new IllegalArgumentException("[ERROR] 프로모션을 등록하는 도중 예외가 발생했습니다.");
-        } else if (startDate.isAfter(endDate)) {
-            throw new IllegalArgumentException("[ERROR] 프로모션을 등록하는 도중 예외가 발생했습니다.");
-        }
-    }
+    int getFreeAmount(int offer);
 }
