f9f134084951a31ef0f177b455eb16b97b39e2e1
GoGradually <chkun3109@gmail.com>
2024-11-10 22:19:32 +0900

    feat(Product.java, Products.java): 상품 정의

---
+++ src/main/java/store/Product.java
@@ -0,0 +1,27 @@
+package store;
+
+public class Product {
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+    private String name;
+    private int amount;
+    private Promotion promotion;
+
+    public Product(String name, int amount, Promotion promotion) {
+        this.name = name;
+        this.amount = amount;
+        this.promotion = promotion;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    public int sellProduct(int offer) {
+        int actualCalculatedAmount = offer;
+        if (actualCalculatedAmount > amount) {
+            actualCalculatedAmount = amount;
+        }
+        amount -= actualCalculatedAmount;
+        return actualCalculatedAmount;
+    }
+}
---
+++ src/main/java/store/Products.java
@@ -0,0 +1,35 @@
+package store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Products {
+    private String name;
+    private int price;
+    private static final List<Product> products = new ArrayList<>();
+
+    public Products(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        products.sort((p, q) -> {
+            if (p.getPromotion() == null) {
+                return 1;
+            } else if (q.getPromotion() == null) {
+                return -1;
+            }
+            return p.getPromotion().getBuy() - q.getPromotion().getBuy();
+        });
+    }
+}
